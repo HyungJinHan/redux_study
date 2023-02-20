60ef99133f1fab7c6df50025eb6c36508dc635fe
HyungJinHan <han1210_36@naver.com>
2023-02-20 16:36:25 +0900

    블로그용 예시 작성

--- src/ex.js
+++ src/ex.js
@@ -24,3 +24,35 @@ console.log(rest);
 // 결과 : { one: '하나', two: '둘' } - rest 안에는 three 값을 제외한 값이 들어있음
 console.log(numberObject);
 // 결과 : { one: '하나', two: '둘', three: '셋' }
+
+const counter = () => {
+  let count = 0;
+
+  // changeCounter는 inner 함수
+  // 객체를 리턴하고 있고, 객체 안에는 increase, decrease, show라는 inner 함수들을 저장
+  function changeCount(number) {
+    count += number;
+  }
+
+  return {
+    increase: function () {
+      changeCount(100);
+    },
+    decrease: function () {
+      changeCount(-10);
+    },
+    show: function () {
+      console.log(count);
+    }
+  }
+};
+
+const counterClosure = counter();
+// counter를 실행하면, outer 함수 스코프를 기억하고 있는 클로저들이 담긴 객체를 반환
+// counterClosure는 counter 함수 내부에 정의된 count나 changeCount에 접근 가능
+
+counterClosure.increase();
+counterClosure.show(); // 100
+
+counterClosure.decrease();
+counterClosure.show(); // 90
